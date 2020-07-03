7253d8b49ec2bd9e292f9619f086e83700cc2d8f
ALRW <awerner1@googlemail.com>
2020-07-03 15:23:40 +0100

    [AW] finished 1.1 exercises

--- chapter_one/exercises.rkt
+++ chapter_one/exercises.rkt
@@ -59,4 +59,48 @@
 ; return 0 in both cases
 
 
+; 1.6
+; It seems to work fine
 
+; 1.7
+
+; with very small numbers the problem is that the number may be smaller than the test precision?
+; For large numbers the number of iterations may effectively leave you in an infinite loop?
+
+(define (average x y)
+  (/ (+ x y ) 2))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (good-enough? guess old-guess)
+  (< (abs (- guess old-guess)) 0.001))
+
+(define (sqrt-iter guess old-guess x)
+  (if (good-enough? guess old-guess)
+    guess
+    (sqrt-iter (improve guess x) guess x)))
+
+(define (sqrt x)
+  (exact->inexact (sqrt-iter 1 0 x)))
+
+(sqrt 4)
+
+; the new method works well for small numbers but not for large
+
+; 1.8
+
+(define (square x) (* x x))
+
+(define (improve-qb guess x)
+  (/ (+ (/ x (square guess)) (* 2 guess)) 3))
+
+(define (qbrt-iter guess old-guess x)
+  (if (good-enough? guess old-guess)
+    guess
+    (qbrt-iter (improve-qb guess x) guess x)))
+
+(define (qbrt x)
+  (exact->inexact (qbrt-iter 1 0 x)))
+
+(qbrt 8)
--- chapter_one/scratch_one_one.rkt
+++ chapter_one/scratch_one_one.rkt
@@ -30,3 +30,36 @@
 
 (define (gte x y)
   (not (< x y)))
+
+; getting a square root
+
+(define (new-if predicate
+                then-clause
+                else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
+
+(define (average x y)
+  (/ (+ x y ) 2))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+    guess
+    (sqrt-iter (improve guess x) x)))
+
+(define (sqrt-iter-b guess x)
+  (new-if (good-enough? guess x)
+          guess
+          (sqrt-iter (improve guess x) x)))
+
+(define (sqrt x)
+  (sqrt-iter 1 x))
+
+(define (sqrt-b x)
+  (sqrt-iter-b 1 x))
