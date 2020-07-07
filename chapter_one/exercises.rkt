a6e9203bd31d368c9ab1fc8e3e241c5ff703add8
ALRW <awerner1@googlemail.com>
2020-07-07 12:01:02 +0100

    [AW] started 1.2

--- chapter_one/exercises.rkt
+++ chapter_one/exercises.rkt
@@ -105,3 +105,40 @@
   (exact->inexact (qbrt-iter 1 0 x)))
 
 (qbrt 8)
+
+; 1.9
+
+;This process is recursive e.g. for (+ 1 2)
+; (inc (+ 0 2))
+; (inc 2)
+; 3
+(define (+ a b)
+  (if (= a 0)
+      b
+      (inc (+ (dec a) b))))
+
+; This is an iterative process .eg. for (+ 1 2)
+; (+ 0 3)
+; 3
+(define (+ a b)
+  (if (= a 0)
+      b
+      (+ (dec a) (inc b))))
+
+; 1.10 Ackermann's function
+
+(define (A x y)
+  (cond ((= y 0) 0)
+        ((= x 0) (* 2 y))
+        ((= y 1) 2)
+        (else (A (- x 1)
+                 (A x (- y 1))))))
+(A 1 10) ;1024
+(A 2 4) ; 65536
+(A 3 3) ; 65536
+
+;(define (f n) (A 0 n)) => 2n
+;(define (g n) (A 1 n)) => 2^n
+;(define (h n) (A 2 n)) => 2^2^n
+
+
